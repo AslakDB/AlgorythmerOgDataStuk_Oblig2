0506a3e1decd26595cf8ba7fae4b4020eaf50651
AslakDB <Askelakken@gmail.com>
2023-09-17 22:07:46 +0200

    Aslak sine 5 timer er over, lykke til jonathan

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+using namespace std;
+
+
+int RecursiveAlgorithm1()
+{
+    
+}
+
+int RecursiveAlgorithm2()
+{
+    
+}
+
+int IterativeAlgorithm()
+{
+    
+}
+
+int main(int argc, char* argv[])
+{
+    return 0;
+    
+}
+
+
